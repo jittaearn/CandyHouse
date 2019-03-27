ccfb8c82deceb3a371d7d1d5cb045e2713fc82bd
Jitta Koopratoomairi <Jitta.k@ku.th>
2019-03-27 11:40:29 +0700

    rename the class candyhouse to BreadWindow

--- candyhouse.py
+++ candyhouse.py
@@ -1,16 +1,19 @@
 import arcade
 
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
+SCREEN_HEIGHT = 800
 
-class CandyHouse(arcade.Window):
+class BreadWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.CREAM)
 
+    def on_draw(self):
+        arcade.start_render()
+
 def main():
-    window = CandyHouse(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = BreadWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
     arcade.run()
  
